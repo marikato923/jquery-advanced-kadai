36cabad51166ff49ae3ce12bc2aae01ffbd15f36
MariKato <marikato923@gmail.com>
2024-09-21 17:49:34 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// 変数の初期化
+let untyped = '' ;
+
+// 必要なHTML要素の習得
+const untypedfieid = $('#untyped');
+
+
+// 複数のテキストを格納する配列
+const textLists = [
+    'Hello World',
+    'This is my app',
+    'How are you?'
+];
+
+// ランダムなテキストを表示
+const createText = function() {
+
+};
+
+// キー入力の判定
+const keyPress = function(e) {
+
+};
+
+// タイピングスキルのランクを判定
+const rankCheck = function(score) {
+
+};
+
+// ゲームを終了
+const gameOver = function(id) {
+    
+};
+
+// カウントダウンタイマー
+const timer = function() {
+
+};
